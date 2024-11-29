600688d9c0afa2fdbfa040d3a351a3d3cb07c7f7
ttonyuuu <jose.souza510@etec.sp.gov.br>
2024-11-28 22:24:37 -0300

    aula de fudamentos

---
+++ aula.js
@@ -0,0 +1,15 @@
+var n1=parseInt(prompt("digite o numero 1"));
+var n2=parseInt(prompt("digite o numero 2"));
+
+if(n1>n2){
+    alert("N1 maior digitado: " + n1);
+
+}else{
+    alert("N2 maior digitado: " + n2);
+
+}
+
+
+
+
+
